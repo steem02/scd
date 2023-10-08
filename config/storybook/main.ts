76c16890f30f8a419a98499b3b99267f2463f181
steem <svsteem@gmail.com>
2023-10-08 23:34:55 +0300

    fix storybook configs

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -26,37 +26,37 @@ const config: StorybookConfig = {
       src: path.join(__dirname, '..', '..', 'src'),
     };
 
-    if (configType === 'DEVELOPMENT') {
-      // Modify config for development
-      if (config.resolve != null) {
-        config.resolve.alias = {
-          '@': paths.src,
-        };
-        config.resolve.extensions?.push('.tsx', '.ts');
-        config.resolve.modules?.push(paths.src);
-      }
-
-      if (config.module?.rules != null) {
-        config.module.rules.push(buildCssLoaders(true));
-      }
-
-      const imageRule = config.module?.rules?.find((rule) => {
-        const test = (rule as { test: RegExp }).test;
-
-        if (test == null) {
-          return false;
-        }
-
-        return test.test('.svg');
-      }) as Record<string, any>;
-
-      imageRule.exclude = /\.svg$/;
-
-      config.module?.rules?.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
+    // if (configType === 'DEVELOPMENT') {
+    // Modify config for development
+    if (config.resolve != null) {
+      config.resolve.alias = {
+        '@': paths.src,
+      };
+      config.resolve.extensions?.push('.tsx', '.ts');
+      config.resolve.modules?.push(paths.src);
     }
+
+    if (config.module?.rules != null) {
+      config.module.rules.push(buildCssLoaders(true));
+    }
+
+    const imageRule = config.module?.rules?.find((rule) => {
+      const test = (rule as { test: RegExp }).test;
+
+      if (test == null) {
+        return false;
+      }
+
+      return test.test('.svg');
+    }) as Record<string, any>;
+
+    imageRule.exclude = /\.svg$/;
+
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    // }
     return config;
   },
 };
