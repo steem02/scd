2e69b42799c08686eabcb328f69483e4cf0359ab
steem <svsteem@gmail.com>
2023-10-10 14:23:52 +0300

    revert i18n for tests

--- src/shared/config/i18n/i18nForTests.ts
+++ src/shared/config/i18n/i18nForTests.ts
@@ -1,34 +1,18 @@
-import { initReactI18next } from 'react-i18next';
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-
-const ns = ['translation', 'sidebar'];
-const supportedLngs = ['en', 'ru'];
-const resources = ns.reduce((acc: any, n) => {
-  supportedLngs.forEach((lng: any) => {
-    if (acc[lng] != null) acc[lng] = {};
-    acc[lng] = {
-      ...acc[lng],
-      // eslint-disable-next-line import/no-dynamic-require, global-require
-      [n]: require(`../../../../public/locales/${lng}/${n}.json`),
-    };
-  });
-  return acc;
-}, {});
+import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(initReactI18next)
-  .use(LanguageDetector)
-  .use(Backend)
   .init({
     lng: 'ru',
     fallbackLng: 'ru',
-    defaultNS: 'translation',
-    interpolation: { escapeValue: false },
-    react: { useSuspense: false },
-    supportedLngs,
-    resources,
+
+    debug: true,
+
+    resources: { en: { translationsNS: {} } },
+  })
+  .catch((e) => {
+    console.log(e.message);
   });
 
 export default i18n;
